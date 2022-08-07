3606d7683334ab6c6d33d4c5e1ca38b80d8c5cd6
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-08-07 15:58:14 +0530

    made tracking service a foreground service

--- app/src/main/java/com/kuluruvineeth/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/runningtracker/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kuluruvineeth.runningtracker.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.kuluruvineeth.runningtracker.R
 import com.kuluruvineeth.runningtracker.db.RunDAO
+import com.kuluruvineeth.runningtracker.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        navigateToTrackingFragmentIfNeeded(intent)
         //setSupportActionBar(toolbar)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
@@ -34,5 +36,18 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?){
+        if(intent?.action == ACTION_SHOW_TRACKING_FRAGMENT){
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+            val navController = navHostFragment.navController
+            navController.navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
