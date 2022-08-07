1bd11dab951813a7d6f6bdb6c7ea61d13aa3c720
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-08-07 16:35:00 +0530

    tracking user location in background

--- app/src/main/java/com/kuluruvineeth/runningtracker/services/TrackingService.kt
+++ app/src/main/java/com/kuluruvineeth/runningtracker/services/TrackingService.kt
@@ -8,24 +8,62 @@ import android.app.PendingIntent.FLAG_MUTABLE
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import com.kuluruvineeth.runningtracker.R
 import com.kuluruvineeth.runningtracker.other.Constants.ACTION_PAUSE_SERVICE
 import com.kuluruvineeth.runningtracker.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.kuluruvineeth.runningtracker.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.kuluruvineeth.runningtracker.other.Constants.ACTION_STOP_SERVICE
+import com.kuluruvineeth.runningtracker.other.Constants.FASTEST_LOCATION_INTERVAL
+import com.kuluruvineeth.runningtracker.other.Constants.LOCATION_UPDATE_INTERVAL
 import com.kuluruvineeth.runningtracker.other.Constants.NOTIFICATION_CHANNEL_ID
 import com.kuluruvineeth.runningtracker.other.Constants.NOTIFICATION_CHANNEL_NAME
 import com.kuluruvineeth.runningtracker.other.Constants.NOTIFICATION_ID
+import com.kuluruvineeth.runningtracker.other.TrackingUtility
 import com.kuluruvineeth.runningtracker.ui.MainActivity
 import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 
 class TrackingService : LifecycleService() {
 
     var isFirstRun = true
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object{
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValues(){
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
@@ -50,7 +88,57 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private fun updateLocationTracking(isTracking: Boolean){
+        if(isTracking){
+            if(TrackingUtility.hasLocationPermission(this)){
+                val request = LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        }else{
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    var locationCallback = object : LocationCallback(){
+        override fun onLocationResult(result: LocationResult?) {
+            super.onLocationResult(result)
+            if(isTracking.value!!){
+                result?.locations?.let { locations ->
+                    for(location in locations){
+                        addPathPoint(location)
+                        Timber.d("NEW LOCATION: ${location.latitude}, ${location.longitude}")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addPathPoint(location: Location?){
+        location?.let {
+            val pos = LatLng(location.latitude,location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
     private fun startForegroundService(){
+        addEmptyPolyline()
+        isTracking.postValue(true)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
             as NotificationManager
 
