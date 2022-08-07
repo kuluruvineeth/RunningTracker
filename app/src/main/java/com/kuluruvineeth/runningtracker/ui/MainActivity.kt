83fa4637bfd8d439357c827a6438c8c1e1eedce9
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-08-07 12:07:17 +0530

    added navigation controller and bottom navigation bar menu.

--- app/src/main/java/com/kuluruvineeth/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/com/kuluruvineeth/runningtracker/ui/MainActivity.kt
@@ -3,18 +3,36 @@ package com.kuluruvineeth.runningtracker.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.kuluruvineeth.runningtracker.R
 import com.kuluruvineeth.runningtracker.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @Inject
-    lateinit var runDAO: RunDAO
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("runDao","RUNDAO: ${runDAO.hashCode()}")
+
+        setSupportActionBar(toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController
+            .addOnDestinationChangedListener{
+                _,destination,_ ->
+                when(destination.id){
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
+
     }
 }
